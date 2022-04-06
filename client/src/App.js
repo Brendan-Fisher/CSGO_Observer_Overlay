25c8411e25dd7cdc58a2a5a88d972a8da810884e
Samuel Marrero <samarreros@gmail.com>
2022-04-06 19:00:51 -0400

    Implemented sockets to transmit game data to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,35 @@
 import { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
-import { queryAPI } from "./api/getGameState";
+import io from "socket.io-client"
 
-
-
-let gameState = null;
-async function getGameState(){
-  console.log("getting gamestate");
-  gameState = await queryAPI();
-  console.log(gameState);
-  setTimeout(200);
-  getGameState();
-}
 
 function App() {
   const [data, setData] = useState(null);
   const [scoreBoard, setSB] = useState(null);
-  const [inGame, setInGame] = useState(null);
+  //const [inGame, setInGame] = useState(null);
+
+  const ENDPOINT = 'http://localhost:5001/'
 
   useEffect(() => {
-    async function getData() {
-      gameState = await queryAPI();
-      console.log(gameState);
-      
-      setData(gameState);  
-      setScoreBoard(gameState);    
-      
-      setInGame(true);
 
-      setTimeout(200);
-      getData();
-    }
-    getData();
+    const socket = io(ENDPOINT)
+
+    socket.on("connect_error", (err) => {
+      console.log(`connect_error due to ${err.message}`);
+    });
+
+    socket.on("update", (update) => {
+      console.log(update)
+      setData(update);
+      setScoreBoard(update);
+      //setInGame(true);
+    });
+
   }, []);
 
   function setScoreBoard(data) {
-
-    if(data.map != undefined){
+    if (data.map != undefined) {
       let scoreboard = {
         phase: data.map.phase,
         round: data.map.round,
@@ -46,12 +38,12 @@ function App() {
         TScore: data.map.team_t.score,
       }
 
-      
-      if(data.phase_countdowns == null){
+
+      if (data.phase_countdowns == null) {
         scoreboard.phase_ends_in = scoreBoard.phase_ends_in;
       }
       else scoreboard.phase_ends_in = data.phase_countdowns.phase_ends_in;
-      
+
       setSB(scoreboard);
     }
     else {
@@ -68,32 +60,32 @@ function App() {
   }
 
 
-    if(data == null || data.map == undefined){
-      return(
-        <div className="App">
-         <div>
-            <p>Awaiting Game</p>
-         </div>
-      </div>
-      )
-    }
-    else {
-      return(
-        <div className="App">
-           <div className="scoreboard">
-              <div className="Team_one">
-                <p id="CT_score">{scoreBoard.CTScore}</p>
-              </div>
-              <div className="Match_info">
-                <p id="time">{parseInt(scoreBoard.phase_ends_in) + 1}</p>
-              </div>
-              <div className="Team_two">
-                <p id="T_score">{scoreBoard.TScore}</p>
-              </div>
-           </div>
+  if (data == null || data.map == undefined || scoreBoard == undefined) {
+    return (
+      <div className="App">
+        <div>
+          <p>Awaiting Game</p>
         </div>
-      )
-    }
+      </div>
+    )
+  }
+  else {
+    return (
+      <div className="App">
+        <div className="scoreboard">
+          <div className="Team_one">
+            <p id="CT_score">{scoreBoard.CTScore}</p>
+          </div>
+          <div className="Match_info">
+            <p id="time">{parseInt(scoreBoard.phase_ends_in) + 1}</p>
+          </div>
+          <div className="Team_two">
+            <p id="T_score">{scoreBoard.TScore}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 };
 
 export default App;
