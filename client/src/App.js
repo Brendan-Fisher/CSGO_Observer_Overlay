2bfbedbd81f5a382881ca8f859cc24530e0e1756
Samuel Marrero <samarreros@gmail.com>
2022-04-07 00:25:52 -0400

    added team ui

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ function App() {
   const [data, setData] = useState(null);
   const [scoreBoard, setSB] = useState(null);
   const [players, setPlayers] = useState(null);
+  const [teamCT, setTeamCT] = useState(null);
+  const [teamT, setTeamT] = useState(null);
   const [currentPlayer, setCurrentPlayer] = useState(null);
   //const [inGame, setInGame] = useState(null);
 
@@ -36,8 +38,8 @@ function App() {
 
   function setPlayersData(data) {
     if (data.map === undefined) return;
-    //console.log(data);
-    setCurrentPlayer(data.player);
+    console.log(data);
+
 
     let playersList = Object.entries(data.allplayers).map(([key, value]) => (
       {
@@ -50,10 +52,16 @@ function App() {
         weapons: value.weapons,
         position: value.position,
         forward: value.forward
-      }));
+      }))
+
+
+    if (playersList.filter(p => p.steamid === data.player.steamid).length > 0) {
+      setCurrentPlayer(data.player);
+    } else setCurrentPlayer(null);
 
     setPlayers(playersList);
-
+    setTeamCT(playersList.filter(p => p.team === "CT"));
+    setTeamT(playersList.filter(p => p.team === "T"));
     //console.log(playersList);
     //console.log(players);
   }
@@ -98,16 +106,46 @@ function App() {
 
   function CurrentPlayer() {
     let player = currentPlayer;
-    if (!player || player.steamid === player.spectarget) return <div></div>;
     console.log(player)
+
+    if (!player) return <div>BALLS</div>;
     return <div>
-      <p>{player.name}</p>
+      <div> {player.name} {player.state.health} {player.state.armor} {player.state.helmet ? "HELMET" : "NO HELMET"} </div>
+      <div>K   A   D   ADR</div>      KILLS
+      <div> {player.match_stats.kills}   {player.match_stats.assists}   {player.match_stats.deaths} {player.state.round_totaldmg}  {player.state.round_kills}</div>
+
     </div>
-  }
+  };
+
+  function TeamCT() {
+    //console.log(players);
+    if (!teamCT) return <div>NUTS</div>
+    return teamCT.map((player, index) => (
+      <tr key={player.observer_slot} style={{ backgroundColor: "rgba(0, 19, 127, 0.35)" }}>
+        <div> {player.match_stats.kills} / {player.match_stats.assists} / {player.match_stats.deaths}</div>
+
+        <div>{player.observer_slot}. {player.name} {player.state.health} {player.state.armor} {player.state.helmet ? "HELMET" : "NO HELMET"} </div>
+
+      </tr>
+    ));
+  };
+
+  function TeamT() {
+    //console.log(players);
+    if (!teamT) return <div>NUTS</div>
+    return teamT.map((player, index) => (
+      <tr key={player.observer_slot} style={{ backgroundColor: "rgba(0, 19, 127, 0.35)" }} >
+        <div> {player.match_stats.kills} / {player.match_stats.assists} / {player.match_stats.deaths}</div>
+
+        <div>{player.observer_slot}. {player.name} {player.state.health} {player.state.armor} {player.state.helmet ? "HELMET" : "NO HELMET"} </div>
+
+      </tr>
+    ));
+  };
 
   if (data == null || data.map == undefined || scoreBoard == undefined) {
     return (
-      <div className="App">
+      <div className="scoreboard">
         <div>
           <p>Awaiting Game</p>
         </div>
@@ -117,7 +155,11 @@ function App() {
   else {
     return (
       <div className="App">
+
+
+
         <div className="scoreboard">
+
           <div className="Team_one">
             <p id="CT_score">{scoreBoard.CTName} {scoreBoard.CTScore}</p>
           </div>
@@ -127,10 +169,21 @@ function App() {
           </div>
           <div className="Team_two">
             <p id="T_score">{scoreBoard.TScore} {scoreBoard.TName}</p>
-          </div>
-        </div>
-      </div>
 
+          </div>
+
+        </div>
+        <div className='currentPlayer'> <CurrentPlayer></CurrentPlayer> </div>
+
+        <div className='CTplayers'> <TeamCT /></div>
+
+        <div className='Tplayers'> <TeamT /></div>
+
+
+
+
+
+      </div>
     )
   }
 };
