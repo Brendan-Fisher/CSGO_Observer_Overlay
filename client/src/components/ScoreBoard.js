3359c31e64b1583691d5803238d9389caa0ad59b
Brendan-Fisher <fisherb1@ufl.edu>
2022-04-09 17:14:59 -0400

    Moved guns into health and subtext bar, other changes

--- client/src/components/ScoreBoard.js
+++ client/src/components/ScoreBoard.js
@@ -1,15 +1,15 @@
 import "./../styles/ScoreBoard.scss";
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
-import { LogoCT, LogoT,Bomb } from "../assets/Icons";
+import { LogoCT, LogoT, Bomb } from "../assets/Icons";
 
 const socket = io("http://localhost:5001");
 function printTime(scoreBoard) {
-  console.log(scoreBoard.phaseInfo)
-  if(scoreBoard.phaseInfo.phase == "bomb" ) {
-    return <img className="bombImage" src={Bomb}></img>
-  }
-  return <div id="time">{scoreBoard.phase_ends_in}</div>
+    console.log(scoreBoard.phaseInfo);
+    if (scoreBoard.phaseInfo.phase == "bomb") {
+        return <img className="bombImage" src={Bomb}></img>;
+    }
+    return <div id="time">{scoreBoard.phase_ends_in}</div>;
 }
 export function ScoreBoard() {
     const [scoreBoard, setSB] = useState(null);
@@ -22,23 +22,42 @@ export function ScoreBoard() {
     if (scoreBoard) {
         return (
             <div className="scoreboard">
-                <div className="TeamLogo">
-                    <img className="teamImage" src={scoreBoard.round < 15 ? LogoCT : LogoT}></img>
+                <div
+                    className="TeamName"
+                    {...(scoreBoard.phaseInfo.phase !== "freezetime"
+                        ? { style: { display: "none" } }
+                        : {})}
+                >
+                    <p>{scoreBoard.round < 15 ? scoreBoard.CTName : scoreBoard.TName}</p>
+                </div>
+                <div className="teamImage">
+                    <img src={scoreBoard.round < 15 ? LogoCT : LogoT}></img>
                 </div>
                 <div className="TeamLeft">
-                    <p id="CT_score">{scoreBoard.round < 15 ? scoreBoard.CTScore : scoreBoard.TScore}</p>
+                    <p id="CT_score">
+                        {scoreBoard.round < 15 ? scoreBoard.CTScore : scoreBoard.TScore}
+                    </p>
                 </div>
                 <div className="Match_info">
-                  {printTime(scoreBoard)}
+                    {printTime(scoreBoard)}
                     <div id="round">ROUND {scoreBoard.round + 1}/30</div>
                 </div>
                 <div className="TeamRight">
-                    <p id="T_score">{scoreBoard.round < 15 ? scoreBoard.TScore : scoreBoard.CTScore}</p>
+                    <p id="T_score">
+                        {scoreBoard.round < 15 ? scoreBoard.TScore : scoreBoard.CTScore}
+                    </p>
                 </div>
-                <div className="TeamLogo">
-                    <img className="teamImage" src={scoreBoard.round < 15 ? LogoT : LogoCT}></img>
+                <div className="teamImage">
+                    <img src={scoreBoard.round < 15 ? LogoT : LogoCT}></img>
                 </div>
-
+                <div
+                    className="TeamName"
+                    {...(scoreBoard.phaseInfo.phase !== "freezetime"
+                        ? { style: { display: "none" } }
+                        : {})}
+                >
+                    <p>{scoreBoard.round < 15 ? scoreBoard.TName : scoreBoard.CTName}</p>
+                </div>
             </div>
         );
     } else {
--- client/src/components/Teams.js
+++ client/src/components/Teams.js
@@ -1,122 +1,138 @@
 import "./../styles/Teams.scss";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import io from "socket.io-client";
 
-import { ArmorHelmet, ArmorFull, Defuse, SmallBomb, Skull, LogoT, LogoCT } from '../assets/Icons';
-import {gunMap} from '../assets/Weapons';
+import { ArmorHelmet, ArmorFull, Defuse, SmallBomb, Skull, LogoT, LogoCT } from "../assets/Icons";
+import { gunMap } from "../assets/Weapons";
 const socket = io("http://localhost:5001");
 
-
-function getPrimaryWeapon(side,player) {
+function getPrimaryWeapon(side, player) {
     //Returns image of primary weapon for a player
     var playerSide;
-    if(side == "L") {
+    if (side === "L") {
         playerSide = "GunL";
-    } else { //Checks the side of the HUD that the player is at
+    } else {
+        //Checks the side of the HUD that the player is at
         playerSide = "GunR";
     }
 
-    if(player.weapons == null) {
-        return (<img className={playerSide} src={gunMap.get("")}></img>);
+    if (player.weapons === null) {
+        return <img alt="Primary Weapon" className={playerSide} src={gunMap.get("")}></img>;
     } //If player somehow doesn't have weapons
     //Don't know what would cause this, but it has led to crashes
-    if(player.weapons.weapon_0 == null) {
-        return (<img className={playerSide} src={gunMap.get("")}></img>);
+    if (player.weapons.weapon_0 == null) {
+        return <img alt="Primary Weapon" className={playerSide} src={gunMap.get("")}></img>;
     }
     //Checks if they have no knife
     //Which shouldn't happen either but this also caused crashes
 
-    if(player.state.health == 0) {
-         return (<img className={playerSide} src={gunMap.get("")}></img>);
+    if (player.state.health === 0) {
+        return <img alt="Primary Weapon" className={playerSide} src={gunMap.get("")}></img>;
     }
     //If player is dead, no need to render any image, just returns a blank png
     var gun;
     var x = "";
     var equipped = true; //Assumes weapon is equipped unless told otherwise
-    Object.keys(player.weapons).forEach(function(key) {
+    Object.keys(player.weapons).forEach(function (key) {
         //Iterates through all weapons of the given player
         gun = player.weapons[key];
-        if(gun.type == "Rifle" || gun.type == "SniperRifle" || gun.type == "Submachine Gun" || gun.type == "Shotgun" || gun.type == "Machine Gun") {
+        if (
+            gun.type === "Rifle" ||
+            gun.type === "SniperRifle" ||
+            gun.type === "Submachine Gun" ||
+            gun.type === "Shotgun" ||
+            gun.type === "Machine Gun"
+        ) {
             //Checks all types of primary weapons to see if it is primary
             //Unfortunately the game doesn't give us a flag saying "primary: yes"
             x = gun.name;
-            if(gun.state != "active") {
+            if (gun.state !== "active") {
                 equipped = false;
                 //If weapon is unequipped, set equipped to false
             }
         }
-
     });
-    if(x == "") { //If we get here, then it means that there were no primary weapons
+    if (x === "") {
+        //If we get here, then it means that there were no primary weapons
         //This loop then checks for any pistols
-        Object.keys(player.weapons).forEach(function(key) {
+        Object.keys(player.weapons).forEach(function (key) {
             gun = player.weapons[key];
-            if(gun.type == "Pistol") {
+            if (gun.type === "Pistol") {
                 x = gun.name;
-                if(gun.state != "active") {//Checks if weapon is equipped
+                if (gun.state !== "active") {
+                    //Checks if weapon is equipped
                     equipped = false;
                 }
             }
         });
     }
-    if(x == "") { //If we get here, then there are no pistols or primary weapons
+    if (x === "") {
+        //If we get here, then there are no pistols or primary weapons
         //Meaning there is only a knife and potentially utility
-        if(player.weapons.weapon_0.state != "active") {
-            playerSide=playerSide + "u";
+        if (player.weapons.weapon_0.state !== "active") {
+            playerSide = playerSide + "u";
         } //If knife is unequipped, change playerSide so a different class will grey out the knife
-        return(<img className={playerSide} src={gunMap.get(player.weapons.weapon_0.name)}></img>);
+        return (
+            <img
+                alt="knife"
+                className={playerSide}
+                src={gunMap.get(player.weapons.weapon_0.name)}
+            ></img>
+        );
         //Return the knife icon, haven't tested yet with non-default knives but it should work
     }
-    if(!equipped) {
-        playerSide=playerSide + "u";
+    if (!equipped) {
+        playerSide = playerSide + "u";
     }
     //Check is equipped, if not, change playerSide so it will be a different class
-    return(<img className={playerSide} src={gunMap.get(x)}></img>); //Return image of weapon
+    return <img alt="weapon" className={playerSide} src={gunMap.get(x)}></img>; //Return image of weapon
 }
-function getSecondaryWeapon(side,player) { //Returns an img containing the secondary weapon of a player
+function getSecondaryWeapon(side, player) {
+    //Returns an img containing the secondary weapon of a player
     var playerSide;
 
-    if(side == "L") {
+    if (side === "L") {
         playerSide = "GunL2";
-    } else { //Get the side of the HUD that the player is on, left or right
+    } else {
+        //Get the side of the HUD that the player is on, left or right
         playerSide = "GunR2";
     }
     //playerSide is the side the player is on the hud, left or right
     //L2 and R2 are classes for the secondary weapons, and are smaller
     //than the primary weapon icons
     var x = ""; //Initialize secondary weapon name to ""
-    if(player.weapons == null) {
-        console.log("a");
-        return (<img className={playerSide} src={gunMap.get("")}></img>);
+    if (player.weapons === null) {
+        //console.log("a");
+        return <img alt="no weapon" className={playerSide} src={gunMap.get("")}></img>;
     }
-    if(player.weapons.weapon_0 == null) {
-        return (<img className={playerSide} src={gunMap.get("")}></img>);
+    if (player.weapons.weapon_0 === null) {
+        return <img alt="no weapon" className={playerSide} src={gunMap.get("")}></img>;
     }
     //Two checks just to make sure nothing crashes,
     //Honestly could probably get rid of one of these and nothing happens
     //Will look into removing later if there are performance issues
 
-    if(!hasPrimary(player)) {
-        return (<img className={playerSide} src={gunMap.get("")}></img>);
+    if (!hasPrimary(player)) {
+        return <img alt="no primary" className={playerSide} src={gunMap.get("")}></img>;
     } //If the player has no primary weapon, exit this function
     //Since the primary weapon in that case would just be their pistol/knife
     var gun;
-    Object.keys(player.weapons).forEach(function(key) {
+    Object.keys(player.weapons).forEach(function (key) {
         gun = player.weapons[key];
-        if(gun.type == "Pistol") { //Searches for a pistol, among all the player's weapons
+        if (gun.type === "Pistol") {
+            //Searches for a pistol, among all the player's weapons
             x = gun.name;
-            if(gun.state == "active") {
+            if (gun.state === "active") {
                 playerSide = playerSide + "e";
             }
             //If the weapon is equipped, add e to playerSide
             //GunR2e and GunL2e are also classes, which are identical to the non-e except they're not as opaque
         }
     });
-    if(gunMap.get(x) != null) {
-        return(<img className={playerSide} src={gunMap.get(x)}></img>); //Accesses the map storing all the gun's icons and returns the image
+    if (gunMap.get(x) !== null) {
+        return <img alt="gun" className={playerSide} src={gunMap.get(x)}></img>; //Accesses the map storing all the gun's icons and returns the image
     }
-    return (<img className={playerSide} src={gunMap.get(x)}></img>); //If you somehow get here, we have a huge problem
-
+    return <img alt="no gun" className={playerSide} src={gunMap.get(x)}></img>; //If you somehow get here, we have a huge problem
 }
 function hasPrimary(player) {
     //Function checks if the player has a primary weapon
@@ -124,9 +140,15 @@ function hasPrimary(player) {
     //There are not many better ways of doing this, sadly
     var primary = false;
     var gun;
-    Object.keys(player.weapons).forEach(function(key) {
+    Object.keys(player.weapons).forEach(function (key) {
         gun = player.weapons[key];
-        if(gun.type == "Rifle" || gun.type == "SniperRifle" || gun.type == "Submachine Gun" || gun.type == "Shotgun" || gun.type == "Machine Gun") {
+        if (
+            gun.type === "Rifle" ||
+            gun.type === "SniperRifle" ||
+            gun.type === "Submachine Gun" ||
+            gun.type === "Shotgun" ||
+            gun.type === "Machine Gun"
+        ) {
             primary = true;
         }
         //Iterates through entire weapon list, and if a primary is found returns true
@@ -157,81 +179,86 @@ export function Teams() {
         //console.log(player);
     });
 
-
     const Team = (props) => {
-        if (!props.team || !props.team[0]) return <div></div>
+        if (!props.team || !props.team[0]) return <div></div>;
 
         let side = props.team === teamLeft ? "L" : "R";
-        let switched = props.team[0].team === "CT" ? "C" : ""
+        let switched = props.team[0].team === "CT" ? "C" : "";
         // console.log(side);
         // console.log(props.team);
         // console.log(props.switched);
 
         return (
-            <div>
-                <div className={side == "L" ? "CTplayers" : "Tplayers"} >
-                    {/* Checks if players are left or right side, misleading variable names */}
-                    {props.team.map((player, index) => (
-                        <div className={side == "L" ? "CTplayerBlock" : "TplayerBlock"} key={player.observer_slot}>
-                            <div className={side == "L" ? "CTArmor" : "TArmor"} id={player.steamid === currentSpec.steamid ? "spec" : ""}>
-                                {player.state.health > 0 ? (
-
-                                    <div>
-                                        <p
-                                            style={{
-                                                color: player.state.health > 50 ? "white" : player.state.health > 20 ? "orange" : "red",
-                                            }}
-                                        >
-                                            {player.state.health}
-                                        </p>
-
-                                        {player.state.helmet ? (
-                                            <ArmorHelmet />
-                                        ) : (
-                                            player.state.armor > 0 && <ArmorFull />
-                                        )}
-                                    </div>
-                                ) : (
-                                    <Skull className="skull" />
-                                )}
-                            </div>
-                            <div className={side == "L" ? "CTplayerInfo" : "TplayerInfo"}>
-                                <div className="healthBarText">
-                                    <div>{getPrimaryWeapon(side,player)}</div>
-                                    <div>{getSecondaryWeapon(side,player)}</div>
-                                    <p className={side == "L" ? "pLeft" : "pRight"}>
-                                        {" "}
-                                        {player.match_stats.kills} / {player.match_stats.assists} /{" "}
-                                        {player.match_stats.deaths} ADR: {player.match_stats.adr}
+            <div className={side === "L" ? "CTplayers" : "Tplayers"}>
+                {/* Checks if players are left or right side, misleading variable names */}
+                {props.team.map((player, index) => (
+                    <div
+                        className={side === "L" ? "CTplayerBlock" : "TplayerBlock"}
+                        key={player.observer_slot}
+                    >
+                        <div
+                            className={side === "L" ? "CTArmor" : "TArmor"}
+                            id={player.steamid === currentSpec.steamid ? "spec" : ""}
+                        >
+                            {player.state.health > 0 ? (
+                                <div>
+                                    <p
+                                        style={{
+                                            color:
+                                                player.state.health > 50
+                                                    ? "white"
+                                                    : player.state.health > 20
+                                                    ? "orange"
+                                                    : "red",
+                                        }}
+                                    >
+                                        {player.state.health}
                                     </p>
 
-                                </div>
-                                <div className={side == "L" ? "CTchart" : "Tchart"}>
-                                    {player.state.health > 0 ? (
-                                        <div
-                                            className={
-                                                switched +
-                                                "Tbar-" +
-                                                player.state.health
-                                            }
-                                        ></div>
+                                    {player.state.helmet ? (
+                                        <ArmorHelmet />
                                     ) : (
-                                        <div className={side == "L" ? "CTbar-D" : "Tbar-D"}></div>
+                                        player.state.armor > 0 && <ArmorFull />
                                     )}
                                 </div>
+                            ) : (
+                                <Skull className="skull" />
+                            )}
+                        </div>
+                        <div className={side === "L" ? "CTplayerInfo" : "TplayerInfo"}>
+                            <div className="healthBarText">
+                                <div>{getPrimaryWeapon(side, player)}</div>
+                                <div>{getSecondaryWeapon(side, player)}</div>
+                                {side === "L" ? (
+                                    <p className="pLeft">
+                                        {player.observer_slot} | {player.name}{" "}
+                                    </p>
+                                ) : (
+                                    <p className="pRight">
+                                        {player.name} | {player.observer_slot}{" "}
+                                    </p>
+                                )}
                             </div>
-
-                            <div className={side == "L" ? "subTextLeft" : "subTextRight"}>
-                                {player.observer_slot}. {player.name}{" "}
+                            <div className={side === "L" ? "CTchart" : "Tchart"}>
+                                {player.state.health > 0 ? (
+                                    <div className={switched + "Tbar-" + player.state.health}></div>
+                                ) : (
+                                    <div className={side === "L" ? "CTbar-D" : "Tbar-D"}></div>
+                                )}
                             </div>
                         </div>
-                    ))}
-                </div>
-            </div>)
-    }
+
+                        <div className={side === "L" ? "subTextLeft" : "subTextRight"}>
+                            {player.match_stats.kills} / {player.match_stats.assists} /{" "}
+                            {player.match_stats.deaths} ADR: {player.match_stats.adr}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
+    };
     if (teamLeft && teamRight) {
         return (
-
             <div>
                 <Team team={teamLeft}></Team>
                 <Team team={teamRight}></Team>
