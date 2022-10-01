c4b6f77dec02977a03378cb80d422a99b6c4f811
Zachary <tysonzachary@ufl.edu>
2022-10-01 12:26:11 -0400

    Converted config into teamInfo.js

---
+++ client/src/config/ReadME.txt
@@ -0,0 +1,13 @@
+Rename team one logo to "teamOneLogo.png"
+
+Rename team two logo to "teamTwoLogo.png"
+
+in config.json, place team one name under after "teamOne": in QUOTES
+same for team two
+
+
+example:
+{
+  "teamOne": "Gator Esports",
+  "teamTwo": "NYU"
+}
---
+++ client/src/teamInfo.js
@@ -0,0 +1,11 @@
+import teamOneLogo from "./config/teamOneLogo.png";
+import teamTwoLogo from "./config/teamTwoLogo.png";
+import cfg from "./config/config.json"
+const teamOneName = cfg.teamOne;
+const teamTwoName = cfg.teamTwo;
+export {
+  teamOneLogo,
+  teamTwoLogo,
+  teamOneName,
+  teamTwoName,
+};
